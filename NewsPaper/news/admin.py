a75cdf48bba5e2b5c156d50cce8ef352c9b3d12c
kctc <kctc@example.ru>
2023-10-29 14:35:26 +0300

    Add file

--- NewsPaper/news/admin.py
+++ NewsPaper/news/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Category
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+
+    list_display = ['title', 'text']
+
+
+admin.site.register(Category)
+admin.site.register(Post, PostAdmin)
 
 
 
